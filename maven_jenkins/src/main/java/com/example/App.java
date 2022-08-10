611f88ac81018308e2954940a5444c4f5debfb9b
Vijayalakshmi.M <Vijayalakshmi.M@EXIND3010-135.allfon.in>
2022-08-10 15:44:39 +0530

    jenkins

--- maven_jenkins/src/main/java/com/example/App.java
+++ maven_jenkins/src/main/java/com/example/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to jenkins!" );
+        System.out.println( "Welcome to jenkins by viji" );
+
     }
 }
